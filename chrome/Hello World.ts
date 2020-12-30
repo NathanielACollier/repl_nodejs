2b90f5a87509f76306ec3e3bac7650db3e8320d4
Nathaniel Collier <drxs3v3n@gmail.com>
2020-12-30 10:13:47 -0600

    Working out more ways to get this to work

--- chrome/Hello World.ts
+++ chrome/Hello World.ts
@@ -1,31 +1,41 @@
 import * as puppeteerUtility from '../modules/puppeteer/utility';
 
 async function main(){
+    let app = new puppeteerUtility.App();
 
-    let puppeteer = await puppeteerUtility.displayHTML(`
+    await app.initWithHtml(`
         <div style='color:green;'>
             <label id='l1'></label>
 
             <br /><button id="b1" type="button">Click Me!</button>
+            <br /><button id='q' type='button'>Quit</button>
         </div>
     `);
 
-
+    app.page.exposeFunction('quitApp', async () => {
+        await app.quit();
+    });
     
-    await puppeteer.page.evaluate(() => {
+    app.page.evaluate(() => {
         let l1 = document.getElementById('l1');
         let b1 = document.getElementById('b1');
+        let quitButton = document.getElementById('q');
     
         let counter = 0;
         b1.addEventListener('click', (ev)=>{
             l1.innerText = `Button clicked ${counter++} times`;
             return false;
         });
+
+        quitButton.addEventListener('click', (ev)=> {
+            (<any>window).quitApp();
+        });
     
         l1.innerText = "Hello World!";
-    
-    
     });
+
+    // should be last line
+    await app.waitForEnd();
 }
 
 main();
--- modules/puppeteer/utility.ts
+++ modules/puppeteer/utility.ts
@@ -1,19 +1,48 @@
 import * as puppeteer from 'puppeteer';
 
-export async function displayHTML(htmlText: string): Promise<{browser: puppeteer.Browser, page: puppeteer.Page}>{
-    let browser = await puppeteer.launch({
-        headless: false,
-        args: ['--app=http://localhost/null']
-    });
-    
-    let page = (await browser.pages())[0];
-    // wait for navigation causes a problem now that we are doing localhost/null
-    //await page.waitForNavigation(); // wait for it to fail to load http://localhost/null
-    
-    await page.setContent(htmlText);
+export class App {
+    browser: puppeteer.Browser;
+    page: puppeteer.Page;
 
-    return {
-        browser: browser,
-        page: page
-    };
+    constructor(){
+
+    }
+
+    async initWithHtml(htmlText: string){
+        this.browser = await puppeteer.launch({
+            headless: false,
+            args: ['--app=http://localhost/null']
+        });
+
+        this.page = (await this.browser.pages())[0];
+
+        // wait for navigation causes a problem now that we are doing localhost/null
+        //await page.waitForNavigation(); // wait for it to fail to load http://localhost/null
+
+        await this.page.setContent(htmlText);
+
+        // setup the way to get the quit signal
+        await this.page.evaluate(() => {
+            (<any>window).programDone = false;
+        });
+    }
+
+
+    async quit(){
+        await this.page.evaluate(() => {
+            (<any>window).programDone = true; // will trigger the program to end
+        });
+    }
+
+    async waitForEnd(){
+        await this.page.waitForFunction(() => {
+            return (<any>window).programDone == true;
+        });
+
+        console.log('Program finished triggered');
+        await this.page.close();
+        await this.browser.close();
+    }
+
+
 }
